4bacc87b0f6d3fffed1c2deb039cf3d7bf0d5556
mikos8 <65290147+mikos8@users.noreply.github.com>
2022-01-04 21:31:09 +0600

    feat:components

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-card width="256" class="mx-0 sidebar">
+    <v-navigation-drawer permanent>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"> Номенклатура </v-list-item-title>
+          <v-list-item-subtitle> </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+            <v-list-item v-for="item in items" :key="item.title" link>
+              <v-list-item-icon>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-icon>
+
+              <v-list-item-content>
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "HelloWorld",
+
+  data(){
+      return {
+          
+      }
+  },
+});
+</script>
