80595ff1450ec2a1c7ec489e79e4ee8959a1601c
mikos8 <65290147+mikos8@users.noreply.github.com>
2022-01-05 02:07:22 +0600

    q

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 import VuetifyDialog from 'vuetify-dialog';
+import 'vuetify-dialog/dist/vuetify-dialog.css';
 
 Vue.use(Vuetify,VuetifyDialog);
 
